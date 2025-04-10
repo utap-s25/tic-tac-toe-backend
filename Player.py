def5bd0d0634cafa206b5f7e66692d8a30b303c5
VisionZ <willzhao2017@gmail.com>
2025-04-09 20:39:21 -0400

    Update Player.py

--- Player.py
+++ Player.py
@@ -4,12 +4,12 @@ from uuid import UUID, uuid4
 
 class Player:
 
-    def __init__(self, player_uuid: str, player_name: str):
-        self.player_uuid = player_uuid
+    def __init__(self, player_id: str, player_name: str):
+        self.player_id = player_id
         self.player_name = player_name
 
     def __str__(self):
-        return json.dumps({"player_uuid": self.player_uuid, "player_name": self.player_name})
+        return json.dumps({"player_id": self.player_id, "player_name": self.player_name})
 
 
 EMPTY_PLAYER = Player(str(uuid4()), "Bob Dylan")
