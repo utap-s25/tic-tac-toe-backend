32da06d51c92db30c8d19d7531a9ffc9d852b0ef
VisionZ <willzhao2017@gmail.com>
2025-04-11 19:49:32 -0400

    Update BoardState.py

--- BoardState.py
+++ BoardState.py
@@ -1,10 +1,20 @@
 import json
 
+STILL_PLAYING = ""
+TIE = "TIE"
+
 
 class BoardState:
 
     def __init__(self, host_player_id: str, guest_player_id: str):
-        self.board = [["", "", ""], ["", "", ""], ["", "", ""]]
+        # [player_id or empty_string, puck_size]
+        self.board = [
+            [["", "0"], ["", "0"], ["", "0"]],
+            [["", "0"], ["", "0"], ["", "0"]],
+            [["", "0"], ["", "0"], ["", "0"]]
+        ]
+        self.host_player_id = host_player_id
+        self.guest_player_id = guest_player_id
         self.pucks_remaining = {
             str(host_player_id): [1, 2, 3, 4, 5, 6],
             str(guest_player_id): [1, 2, 3, 4, 5, 6]
@@ -15,7 +25,16 @@ class BoardState:
         Tie means game is over and tied
         Otherwise, it will be the user_id of the winning player
         """
-        self.game_over = ""
+        self.game_over = STILL_PLAYING
+
+    def get_current_player(self):
+        return self.turn
+
+    def get_waiting_player(self):
+        for player in self.pucks_remaining.keys():
+            if player != self.turn:
+                return player
+        return None
 
     def __str__(self):
         return json.dumps({
